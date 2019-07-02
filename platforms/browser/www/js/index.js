45163e3e8ed1b4e837dfeb13c1e70ab3704f4945
sanket064 <48606892+sanket064@users.noreply.github.com>
2019-07-02 09:06:02 -0700

    solution 8

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -37,6 +20,57 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        var db;
+        $(document).ready(function(){
+             
+            $("#saveNewContact").bind( "tap", tapHandler );
+         
+            function tapHandler( event ){
+                insertRow();
+                $("body").pagecontainer("change", "#home");
+            }
+        
+            function insertRow(){
+                let lsContacts = localStorage.getItem('contacts');
+                if(lsContacts!== null){
+                
+                    lsContacts = JSON.parse(lsContacts);
+                }else{
+                    lsContacts = new Array();
+                }
 
+                let newRecord = {
+                    "contactName": $("#contactName").val(),
+                    "contactEmail": $("#contactEmail").val()
+                }
+                lsContacts.push(newRecord);
+                $("#contactName").val("");
+                $("#contactEmail").val("");
+
+                localStorage.setItem('contacts', JSON.stringify(lsContacts));
+            }
+        
+            $(document).on( 'pagebeforeshow' , '#home' ,function(event){
+                let lsContacts = localStorage.getItem('contacts');
+                lsContacts = JSON.parse(lsContacts);
+                displayContacts(lsContacts);
+            });
+        
+            function displayContacts(results){
+                var list = $("#contactListLi");
+                list.empty();
+        
+                console.log(results);
+                var len = results.length, i;
+                for (i = 0; i < len; i++) {
+                    // list.append("<li><a href=\"#\">" + results[i].contactName + "</li>");
+                    list.append(`<li><a href="#"> ${results[i].contactName} </li>`);
+                    console.log(results[i].contactName);
+                }
+        
+                $("#contactListLi").listview("refresh");
+                
+            }
+        });      
     }
 };
